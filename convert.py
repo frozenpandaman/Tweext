4b723a70d622550263711a2a3375a4978476432f
eli <frozenpandaman@gmail.com>
2015-10-23 03:41:27 -0700

    cleaned up code, added title/author support, with jack london as our example

--- convert.py
+++ convert.py
@@ -4,6 +4,9 @@ def convert(file):
 	infile = open(file,'r')
 	filename = file.replace(".txt", '')
 
+	title = "Call of the Wild" # test
+	author = "Jack London"
+
 	contents = infile.read()
 	infile.close()
 	contents = contents.split("\n\n") # split up paragraphs
@@ -16,21 +19,13 @@ def convert(file):
 
 	outfile = open(filename + ".tw",'w')
 
-	for i in range(len(paras)): # append Twee paragraph headings
-		outfile.write(":: " + str(heading(i)) + "\n")
-
-		# format links at end of paragraph
-		words = paras[i].split(' ')
-		lastword = words[-1]
-		for word in words:
-			if word == lastword and i != len(paras)-1:
-				# if it's the last word, use twine link syntax, accounting for the period
-				# and case for dealing with last para
-				outfile.write("[[" + word[:-1] + "|" + str(i+1) + "]].")
-			else:
-				outfile.write(word + " ")
-
+	for i in range(len(paras)):
+		outfile.write(":: " + str(heading(i)) + "\n") # append Twee paragraph headings
+		formatLinks(paras, i, outfile) # make the last word of each paragraph a link
 		outfile.write("\n\n")
+
+	setTitle(title, outfile)
+	setAuthor(author, outfile)
 
 	outfile.close()
 
@@ -39,5 +34,26 @@ def heading(n):
 		return "Start"
 	else:
 		return n
+
+def formatLinks(paras, i, outfile):
+	words = paras[i].split(' ')
+	lastword = words[-1]
+	for word in words:
+		if word == lastword and i != len(paras)-1:
+			# if it's the last word, make it a link to the next paragraph
+			# (using tiddlywiki syntax), accounting for the period
+			# stuff after the 'and' for dealing with last para
+			outfile.write("[[" + word[:-1] + "|" + str(i+1) + "]].")
+		else:
+			outfile.write(word + " ")
+
+def setTitle(title, outfile):
+	outfile.write(":: StoryTitle\n")
+	outfile.write(title + "\n\n")
+
+def setAuthor(author, outfile):
+	outfile.write(":: StoryAuthor\n")
+	outfile.write(author + "\n\n")
+
 
 convert(sys.argv[1])
